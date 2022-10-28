f02aea3124e4b0a76a09c8e6a6f14da185cbbb48
Erick Hernandez <yerickk8@gmail.com>
2022-10-28 14:17:42 -0500

    Implemented listing food

---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/Food';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  foods: Food[] = [];
+  constructor(private fooService: FoodService) {
+    this.foods = fooService.getAll();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
