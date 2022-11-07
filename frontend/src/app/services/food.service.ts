11e7bff404e700c048c3c63a1f45c47be44affc0
Erick Hernandez <yerickk8@gmail.com>
2022-11-07 13:19:14 -0600

    Implemented getAllFoodsBySearchTerm function and added search bar component

--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -9,7 +9,11 @@ export class FoodService {
 
   constructor() { }
 
-  getAll():Food[]{
+  getAll(): Food[] {
     return sample_foods
+  }
+
+  getAllFoodsBySearchTerm(searchTerm: string) {
+    return this.getAll().filter(food => food.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()))
   }
 }
